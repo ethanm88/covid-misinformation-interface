3f2bf2ba5a374bb053b7d8796df4325a292ec3c7
Yao Dou <douy@cs.washington.edu>
2022-07-14 17:30:16 -0400

    version 1

--- js/index.js
+++ js/index.js
@@ -8,58 +8,120 @@ function getcolor(color1, color2, percentage) {
     var r2 = parseInt(color2.substring(1, 3), 16);
     var g2 = parseInt(color2.substring(3, 5), 16);
     var b2 = parseInt(color2.substring(5, 7), 16);
-    console.log(r1, g1, b1, r2, g2, b2);
     var r = Math.round((r1 + (r2 - r1) * percentage)).toString(16);
     var g = Math.round((g1 + (g2 - g1) * percentage)).toString(16);
     var b = Math.round((b1 + (b2 - b1) * percentage)).toString(16);
-    console.log(r, g, b);
     return "#" + (r.length == 1 ? "0" + r : r) + (g.length == 1 ? "0" + g : g) + (b.length == 1 ? "0" + b : b);
 }
 
-var filename = "";
+function salience_map(tokens, scores) {
+    var html = "";
+    for (var i = 0; i < tokens.length; i++) {
+        // get the word from the array
+        var word = tokens[i];
+        var score = scores[i];
+        var color = getcolor(color1, color2, score);
+        if (score > 0.85) {
+            html += "<span style='color:white; background-color:" + color + "'>" + word + "</span> ";
+        } else {
+            html += "<span style='background-color:" + color + "'>" + word + "</span> ";
+        }
+        
+    }
+    return html;
+}
+
+var filename = "https://raw.githubusercontent.com/Yao-Dou/covid-misinformation-interface/main/data/covid.csv";
+var round_index = 1;
+
+var annotations = {};
 
 $(document).ready(function() {
     Papa.parse(filename, {
         worker: true,
         download: true,
         complete: function (results) {
+            function display_ith_example(i) {
+                var data = results.data;
+                var example = data[i];
+                var tweet = example[0];
+                var treatment = example[1];
+                var confidence = example[3]
+                $("#confidence-span").html(confidence+"%");
+                var scores = example[4].split(" ");
+                // scores to float
+                for (var i = 0; i < scores.length; i++) {
+                    scores[i] = parseFloat(scores[i]);
+                }
+                $("#tweet").html(salience_map(tweet.split(" "), scores));
+                $("#treatment-span").html(treatment);
+                $("#treatment-span-2").html(treatment);
+                // check if round_index is in annotations
+                if (annotations[round_index] != undefined) {
+                    annotation = annotations[round_index];
+                    var $radios = $('input:radio[name=violation]');
+                    $radios.filter("[value=" + annotation +"]").prop('checked', true);
+                } else {
+                    $("input:radio[name=violation]").prop('checked', false);
+                }
+                $("#guidelines").fadeOut(0.2)
+                $("#guidelines-button").html("Guidelines");
+                // console.log(scores);
+            }
+            display_ith_example(1);
+            $("#prev").on("click", function(e) {
+                round_index--;
+                if (round_index < 1) {
+                    round_index = 1;
+                }
+                $("#index-span").html(round_index);
+                display_ith_example(round_index)
+            });
 
+            $("#next").on("click", function(e) {
+                round_index++;
+                if (round_index > results.data.length - 1) {
+                    round_index = results.data.length - 1;
+                }
+                $("#index-span").html(round_index);
+                display_ith_example(round_index)
+            });
+    
+            $('input[type=radio][name=violation]').change(function() {
+                annotations[round_index] = this.value;
+                console.log(annotations);
+            });
+
+            $("#guidelines-button").on("click", function(e) {
+                var left  = e.pageX - 60  + "px";
+                var top  = e.pageY + 25 + "px";
+                var div = document.getElementById("guidelines");
+                div.style.left = left;
+                div.style.top = top;
+                if (div.style.display == "" || div.style.display == "none") {
+                    $("#guidelines").fadeIn(0.2)
+                    $(this).html("Close");
+                } else {
+                    $("#guidelines").fadeOut(0.2)
+                    $(this).html("Guidelines");
+                }
+                return false;
+            });
+
+            $("#submit-button").on("click", function(e) {
+                // download the annotations as json
+                var json = JSON.stringify(annotations);
+                var blob = new Blob([json], {type: "application/json"});
+                var url = URL.createObjectURL(blob);
+                var a = document.createElement("a");
+                a.href = url;
+                a.download = "annotations.json";
+                document.body.appendChild(a);
+                a.click();
+                document.body.removeChild(a);
+                URL.revokeObjectURL(url);
+            });
+
+            $( "#guidelines" ).draggable();
         }});
-
-    $("#submit").on("click", function(e) {
-        var textInput = $("#textInput").val();
-        var scoreInput = $("#scoreInput").val();
-        if (textInput === "" || scoreInput === "") {
-            $("#salience-map").html("<span class='bg-gray'>&nbsp;Missing text input or score input &nbsp;</span>");
-        } else {
-            // split the input into an array of words
-            var words = textInput.split(" ");
-            var scores = scoreInput.split(" ");
-            if (words.length != scores.length) {
-                $("#salience-map").html("<span class='bg-gray'>&nbsp;Text input and score input must be the same length &nbsp;</span>");
-                return;
-            }
-            // transform scores to float
-            for (var i = 0; i < scores.length; i++) {
-                scores[i] = parseFloat(scores[i]);
-            }
-            // normalize scores
-            var max = Math.max.apply(null, scores);
-            var min = Math.min.apply(null, scores);
-            for (var i = 0; i < scores.length; i++) {
-                scores[i] = (scores[i] - min) / (max - min);
-            }
-            console.log(scores);
-            var html = "";
-            for (var i = 0; i < words.length; i++) {
-                // get the word from the array
-                var word = words[i];
-                var score = scores[i];
-                var color = getcolor(color1, color2, score);
-                // create the html for the word
-                html += "<span style='background-color:" + color + "'>&nbsp;" + word + "&nbsp;</span> ";
-            }
-            $("#salience-map").html(html);
-        }
-    });
 });
